45288a8ded1b0ae8ba0d425673e12189b117419e
ducanh215204 <anhldse183815@fpr.edu.vn>
2026-01-31 16:40:29 +0700

    check

--- src/components/GroupBuyingBanner.tsx
+++ src/components/GroupBuyingBanner.tsx
@@ -15,7 +15,7 @@ export const GroupBuyingBanner: React.FC = () => {
           </p>
           <Button
             size="lg"
-            className="bg-white text-green-500 hover:bg-gray-600 font-semibold"
+            className="bg-white text-green-600 hover:bg-gray-100 font-semibold"
           >
             Khám phá ngay
           </Button>
